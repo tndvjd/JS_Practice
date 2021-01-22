dab60812f1192226eec728c4faa9a346ec3fccfd
Hyperidiot <tndvjd@gmail.com>
2021-01-22 22:15:16 +0900

    update POP CAT

--- popCatMeme/main.js
+++ popCatMeme/main.js
@@ -1,7 +1,16 @@
 const popCat = document.querySelector("img");
 const score = document.querySelector("span");
+const resetBtn = document.querySelector("button");
+
+var audio = new Audio("audio/pop.mp3")
 
 let popCount = 0;
+
+function rotateCat() {
+  let randomNum = Math.round(Math.random());
+  Boolean(randomNum) === true ? popCat.classList.add("rotate") : popCat.classList.remove("rotate");
+}
+
 
 function saveScore(popCount) {
   localStorage.setItem("count", popCount);
@@ -9,13 +18,14 @@ function saveScore(popCount) {
 
 function loadScore() {
   popCount = localStorage.getItem("count");
-  score.innerText = `Your POP Count : ${localStorage.getItem("count")}`;
+  localStorage.getItem("count") === null ? score.innerText = 'Press P and record your score!' : score.innerText = `Your POP Count : ${localStorage.getItem("count")}`;
 }
 
 function keyDown(event) {
-  console.log(event);
   if (event.code === "KeyP") {
     popCat.src = "images/pop.png";
+    audio.play();
+    rotateCat();
   }
 }
 
@@ -31,6 +41,11 @@ function keyUp(event) {
 function init() {
   window.onkeydown = (e) => keyDown(e);
   window.onkeyup = (e) => keyUp(e);
+  resetBtn.addEventListener('click', (e) => {
+    localStorage.removeItem('count');
+    popCount = 0;
+    score.innerText = 'Press P and record your score!'
+  })
   loadScore();
 }
 
